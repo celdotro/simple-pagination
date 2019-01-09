b016d939bd0506b69d083e046ebf1721bb832e5a
Florin D <fredtux@users.noreply.github.com>
2019-01-09 14:31:04 +0200

    Fixed page change bug

--- src/components/SimplePagination.vue
+++ src/components/SimplePagination.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <div v-if="pages < maxPages">
-      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="changePage(1)"> << </button>
-      <button v-for="item in pages" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')">{{item}}</button>
-      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="changePage(pages)">>></button>
+      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="handlePageChange(1)"> << </button>
+      <button v-for="item in pages" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')" @click="handlePageChange(pages)">{{item}}</button>
+      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="handlePageChange(pages)">>></button>
     </div>
     <div v-else>
-      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="changePage(1)"> << </button>
+      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="handlePageChange(1)"> << </button>
 
-      <button v-for="item in leftPages" @click="changePage(item)" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"">{{item}}</button>
+      <button v-for="item in leftPages" @click="handlePageChange(item)" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"">{{item}}</button>
       <span v-if="(pages - 4) - currPage > 3">...</span>
-      <button v-for="item in [pages - 4, pages - 3, pages - 2, pages - 1, pages]" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"" :key="item" @click="changePage(item)">{{item}}</button>
+      <button v-for="item in [pages - 4, pages - 3, pages - 2, pages - 1, pages]" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"" :key="item" @click="handlePageChange(item)">{{item}}</button>
 
-      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="changePage(pages)">>></button>
+      <button :class="vspbutton + (vspbuttonfast !== '' ? ' ' + vspbuttonfast : '')" @click="handlePageChange(pages)">>></button>
     </div>
   </div>
 </template>
@@ -49,7 +49,7 @@
       }
     },
     methods: {
-      changePage(item){
+      handlePageChange(item){
         this.currPage = item
 		this.generateLeftPages()
 		this.$emit('changePage', item)
