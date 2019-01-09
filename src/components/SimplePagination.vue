b0da8ac4f21e87a0b879bab16c5127494b165f0e
Florin D <fredtux@users.noreply.github.com>
2019-01-09 13:17:10 +0200

    Fixed classes

--- src/components/SimplePagination.vue
+++ src/components/SimplePagination.vue
@@ -2,15 +2,15 @@
   <div>
     <div v-if="pages < maxPages">
       <button :class="vspbutton" @click="changePage(1)"> << </button>
-      <button v-for="item in pages" :key="item" :class="vspbutton + ' ' + (item == currPage ? vspbuttonselected : '')">{{item}}</button>
+      <button v-for="item in pages" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')">{{item}}</button>
       <button :class="vspbutton" @click="changePage(pages)">>></button>
     </div>
     <div v-else>
       <button :class="vspbutton" @click="changePage(1)"> << </button>
 
-      <button v-for="item in leftPages" @click="changePage(item)" :key="item" :class="vspbutton + ' ' + (item == currPage ? vspbuttonselected : '')"">{{item}}</button>
+      <button v-for="item in leftPages" @click="changePage(item)" :key="item" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"">{{item}}</button>
       <span v-if="(pages - 4) - currPage > 3">...</span>
-      <button v-for="item in [pages - 4, pages - 3, pages - 2, pages - 1, pages]" :class="vspbutton + ' ' + (item == currPage ? vspbuttonselected : '')"" :key="item" @click="changePage(item)">{{item}}</button>
+      <button v-for="item in [pages - 4, pages - 3, pages - 2, pages - 1, pages]" :class="vspbutton + (item == currPage ? ' ' + vspbuttonselected : '')"" :key="item" @click="changePage(item)">{{item}}</button>
 
       <button :class="vspbutton" @click="changePage(pages)">>></button>
     </div>
